d89f3d9369c1aed13c4b7ecc85d463f18b700692
Bruzzlee <chelilea@students.zhaw.ch>
2011-04-13 18:42:41 +0200

    gitignore modification for control_panel.xml - Benchmark-Tool including deadband

--- sw/airborne/modules/benchmark/flight_benchmark.c
+++ sw/airborne/modules/benchmark/flight_benchmark.c
@@ -28,6 +28,11 @@
 float SquareSumErr_airspeed;
 float SquareSumErr_altitude;
 float SquareSumErr_position;
+float ToleranceAispeed;
+float ToleranceAltitude;
+float TolerancePosition;
+bool_t benchm_reset;
+
 
 //uint8_t numOfCount;
 
@@ -37,23 +42,44 @@ void flight_benchmark_init( void ) {
 	SquareSumErr_airspeed = 0;
 	SquareSumErr_altitude = 0;
 	SquareSumErr_position = 0;
+	ToleranceAispeed = BENCHMARK_TOLERANCE_AIRSPEED;
+	ToleranceAltitude = BENCHMARK_TOLERANCE_ALTITUDE;
+	TolerancePosition = BENCHMARK_TOLERANCE_POSITION;	
+	benchm_reset = 0;
 }
 
 void flight_benchmark_periodic( void ) {
 	float err_temp = 0;
+	
+	if (benchm_reset){
+		flight_benchmark_reset();
+		benchm_reset = 0;
+	}
 #ifdef BENCHMARK_AIRSPEED
 	err_temp = estimator_airspeed - v_ctl_auto_airspeed_setpoint;
-	SquareSumErr_airspeed += (err_temp * err_temp);
+	err_temp = fabs(err_temp);
+	if (err_temp>ToleranceAispeed){
+		err_temp = err_temp-ToleranceAispeed;
+		SquareSumErr_airspeed += (err_temp * err_temp);
+	}
+	
 #endif
 	
 #ifdef BENCHMARK_ALTITUDE
 	err_temp = estimator_z - v_ctl_altitude_setpoint;
-	SquareSumErr_altitude += (err_temp * err_temp);
+	err_temp = fabs(err_temp);
+	if (err_temp>ToleranceAltitude){
+		err_temp = err_temp-ToleranceAltitude;
+		SquareSumErr_altitude += (err_temp * err_temp);
+	}
 #endif
 	
 #ifdef BENCHMARK_POSITION
 // 	err_temp = waypoints[target].x - estimator_x;
-	SquareSumErr_position += (nav_shift * nav_shift);
+	if (nav_shift>TolerancePosition){
+		err_temp = nav_shift-TolerancePosition;
+		SquareSumErr_position += (err_temp * err_temp);
+	}
 #endif
 	
 	DOWNLINK_SEND_FLIGHT_BENCHMARK(DefaultChannel, &SquareSumErr_airspeed, &SquareSumErr_altitude, &SquareSumErr_position)
--- sw/airborne/modules/benchmark/flight_benchmark.h
+++ sw/airborne/modules/benchmark/flight_benchmark.h
@@ -7,4 +7,9 @@ void flight_benchmark_init( void );
 void flight_benchmark_periodic( void );
 void flight_benchmark_reset( void );
 
+extern float ToleranceAispeed;
+extern float ToleranceAltitude;
+extern float TolerancePosition;
+extern bool_t benchm_reset;
+
 #endif /* FLIGHTBENCHMARK_H */
